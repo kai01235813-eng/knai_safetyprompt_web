ffeb9a123e0dad8d85b4939d7008095921440f66
kai01235813-eng <kai01235813@gmail.com>
2026-01-16 11:13:50 +0900

    Fix CORS: Disable credentials to allow wildcard origin

--- api/main.py
+++ api/main.py
@@ -27,7 +27,7 @@ app = FastAPI(title="KEPCO Prompt Security API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
